ea0b016b8c370d6d0ecf2fab22960704a5e9c409
BugulnikList <Bugulniklist@gmail.com>
2024-11-14 21:28:18 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+# Ирина Старунина Финальный проект Инженер по тестированию плюс 23 когорта
+BASE_URL = 'https://0b0cf467-4e0d-4859-8669-24548ca2c77a.serverhub.praktikum-services.ru/'
+CREATE_ORDERS = '/api/v1/orders'
+ORDER_INF = '/api/v1/orders/track?t='
